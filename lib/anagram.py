3bce6cc00471e81f19291ef3bca5430fe5829d7b
wendymasese <wendy.masese@student.moringaschool.com>
2024-07-08 17:21:16 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        return [word for word in words if sorted(self.word) == sorted(word)]
